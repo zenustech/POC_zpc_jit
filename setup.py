f477d8cce89ef76698f6344a1465c0082434076e
dwxrycb123 <njxydwx@126.com>
2024-03-11 21:27:17 +0800

    add zeno mode installation handling

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 import os
 import sys
-from glob import glob 
+from glob import glob
 import pathlib
 import subprocess
 import shutil
 from os.path import dirname, join as pjoin
 from setuptools import setup, find_packages
+
+ZENO_BIN_DIR = os.environ.get("ZENO_BIN_DIR")
+IS_ZENO_MODE = (ZENO_BIN_DIR is not None) and (len(ZENO_BIN_DIR) > 0)
 
 
 def find_file_dir_recursive(name, path):
@@ -15,82 +18,106 @@ def find_file_dir_recursive(name, path):
 
 
 meta = {}
-with open(pjoin('pyzpc', '__version__.py')) as f:
+with open(pjoin("pyzpc", "__version__.py")) as f:
     exec(f.read(), meta)
-
 cwd = pathlib.Path().absolute()
 base_dir = dirname(__file__)
-cmake_dir = os.path.join(base_dir, 'pyzpc', 'zpc_jit')
-os.chdir(cmake_dir)
-build_dir = pjoin(base_dir, 'cmake_build')
-out_lib_dir = pjoin(cmake_dir, 'lib')
-out_lib_dir_path = pathlib.Path(out_lib_dir)
-out_lib_dir_path.mkdir(parents=True, exist_ok=True)
-cmake_args = [
-    f'-B{build_dir}',
-    '-DCMAKE_BUILD_TYPE=Release',
-    '-DZS_ENABLE_ZENO_CU_WRANGLE=OFF',
-    '-DZS_ENABLE_VULKAN=OFF',
-    '-DZS_ENABLE_JIT=ON',
-    '-DZS_ENABLE_CUDA=ON',
-    # '-DCMAKE_TOOLCHAIN_FILE=C:/Develop/vcpkg/scripts/buildsystems/vcpkg.cmake',
-    '-DZS_BUILD_SHARED_LIBS=ON'
-]
-build_args = [
-    '--config', 'Release',
-    '--parallel', str(os.cpu_count())
-]
-subprocess.run(
-    ['cmake', cmake_dir, *cmake_args], cwd='.', check=True
-)
-subprocess.run(
-    ['cmake', '--build', build_dir, *build_args], cwd='.', check=True
-)
+cmake_dir = os.path.join(base_dir, "pyzpc", "zpc_jit")
+out_lib_dir = pjoin(cmake_dir, "lib")
 
-lib_prefix = '' if os.name == 'nt' else 'lib'
-lib_suffix = 'so'
-if sys.platform == 'win32':
-    lib_suffix = 'dll'
-elif sys.platform == 'darwin':
-    lib_suffix = 'dylib'
-loc_lib_name = f'{lib_prefix}zpc_py_interop.{lib_suffix}'
-build_lib_dir = find_file_dir_recursive(loc_lib_name, build_dir)
-shared_lib_paths = glob(pjoin(build_dir, '**/*.so'), recursive=True) + \
-    glob(pjoin(build_dir, '**/*.dll'), recursive=True) + \
-    glob(pjoin(build_dir, '**/*.dylib'), recursive=True)
-for path in shared_lib_paths:
-    filename = os.path.basename(path)
-    shutil.copy(
-        path, 
-        pjoin(out_lib_dir, filename)
+if not IS_ZENO_MODE:
+    os.chdir(cmake_dir)
+    build_dir = pjoin(base_dir, "cmake_build")
+    out_lib_dir_path = pathlib.Path(out_lib_dir)
+    out_lib_dir_path.mkdir(parents=True, exist_ok=True)
+    cmake_args = [
+        f"-B{build_dir}",
+        "-DCMAKE_BUILD_TYPE=Release",
+        "-DZS_ENABLE_ZENO_CU_WRANGLE=OFF",
+        "-DZS_ENABLE_VULKAN=OFF",
+        "-DZS_ENABLE_JIT=ON",
+        "-DZS_ENABLE_CUDA=ON",
+        # '-DCMAKE_TOOLCHAIN_FILE=C:/Develop/vcpkg/scripts/buildsystems/vcpkg.cmake',
+        "-DZS_BUILD_SHARED_LIBS=ON",
+    ]
+    build_args = ["--config", "Release", "--parallel", str(os.cpu_count())]
+    subprocess.run(["cmake", cmake_dir, *cmake_args], cwd=".", check=True)
+    subprocess.run(
+        ["cmake", "--build", build_dir, *build_args], cwd=".", check=True
     )
+
+dynamic_lib_names = [
+    "clang",
+    "cudart",
+    "cuda",
+    "nvrtc",
+    "omp",
+    "python3.10",
+    "zpccore",
+    "zpccuda",
+    "zpc_jit_clang",
+    "zpc_jit_nvrtc",
+    "zpcomp",
+    "zpc_py_interop",
+    "zpc",
+    "zpctool",
+    "zspartio",
+    "zswhereami",
+]
+
+lib_prefix = "" if os.name == "nt" else "lib"
+lib_suffix = "so"
+if sys.platform == "win32":
+    lib_suffix = "dll"
+elif sys.platform == "darwin":
+    lib_suffix = "dylib"
+dynamic_lib_names = [
+    f"{lib_prefix}{name}.{lib_suffix}" for name in dynamic_lib_names
+]
+
+if not IS_ZENO_MODE:
+    loc_lib_name = f"{lib_prefix}zpc_py_interop.{lib_suffix}"
+    build_lib_dir = find_file_dir_recursive(loc_lib_name, build_dir)
+    shared_lib_paths = (
+        glob(pjoin(build_dir, "**/*.so"), recursive=True)
+        + glob(pjoin(build_dir, "**/*.dll"), recursive=True)
+        + glob(pjoin(build_dir, "**/*.dylib"), recursive=True)
+    )
+    for path in shared_lib_paths:
+        filename = os.path.basename(path)
+        shutil.copy(path, pjoin(out_lib_dir, filename))
+else:
+    for name in os.listdir(ZENO_BIN_DIR):
+        if name in dynamic_lib_names:
+            shutil.copy(pjoin(ZENO_BIN_DIR, name), pjoin(out_lib_dir, name))
+
 # os.removedirs(build_dir)
 os.chdir(str(cwd))
 
 setup(
-    name=meta['__title__'],
-    version=meta['__version__'],
-    url=meta['__url__'],
-    description=meta['__description__'],
+    name=meta["__title__"],
+    version=meta["__version__"],
+    url=meta["__url__"],
+    description=meta["__description__"],
     long_description=(
-        'More information could be found at https://github.com/zenustech/POC_zpc_jit'),
-    platforms=['any'],
+        "More information could be found at https://github.com/zenustech/POC_pyzpc"
+    ),
+    platforms=["any"],
     packages=find_packages(),
     package_data={
-        "": ['zpc_jit/zpc/**/*.hpp',
-             'zpc_jit/zpc/**/*.h',
-             'zpc_jit/zpc/**/*.cuh',
-             'zpc_jit/zpc/**/*.cpp',
-             'zpc_jit/zpc/**/*.cu',
-             'zpc_jit/lib/*.so',
-             'zpc_jit/lib/*.dll']
+        "": [
+            "zpc_jit/zpc/**/*.hpp",
+            "zpc_jit/zpc/**/*.h",
+            "zpc_jit/zpc/**/*.cuh",
+            "zpc_jit/zpc/**/*.cpp",
+            "zpc_jit/zpc/**/*.cu",
+            "zpc_jit/lib/*.so",
+            "zpc_jit/lib/*.dll",
+        ]
     },
     include_package_data=True,
     classifiers=[],
     tests_require=[],
-    install_requires=[
-        'numpy',
-        'sympy'
-    ],
-    python_requires='>=3.8'
+    install_requires=["numpy", "sympy"],
+    python_requires=">=3.8",
 )
